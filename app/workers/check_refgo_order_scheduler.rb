269707afdc7bac642f83b225f44cebe0ac288620
stap780 <panaet80@mail.ru>
2024-07-17 16:58:10 +0300

    change order worker

--- app/workers/check_refgo_order_scheduler.rb
+++ app/workers/check_refgo_order_scheduler.rb
@@ -4,7 +4,7 @@ class CheckRefgoOrderScheduler
   include Sidekiq::Worker
 
   def perform
-    orders = Order.all.limit(100)
+    orders = Order.order(id: :desc).limit(100)
     orders.each do |order|
         CheckRefgoJob.perform_later(order.id)
     end
